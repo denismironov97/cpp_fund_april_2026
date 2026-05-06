fc7466c71b7a82bfcc96374c25f47bed06364135
Denis M <denis.m.031997@gmail.com>
2026-05-06 17:35:20 +0300

    improved task#6 crafted two solutions

--- arrays/exes/ex6.cpp
+++ arrays/exes/ex6.cpp
@@ -6,25 +6,32 @@ using namespace std;
 
 void readArray(int arr[], int arr_length);
 int getGeneralTerm(int arr_source_length);
-void calcAbsOffsetBetweenArrElems(int *point_arr_source, int arr_source_length, long long *point_destination_arr);
+void calcAbsOffsetBetweenArrElems(int *point_arr_source, int arr_source_length, int *point_destination_arr);
 void printArr(int arr_source[], int arr_length);
-long long getMinIntValue(long long arr_source[], int arr_source_length);
+int getMinIntValue(int arr_source[], int arr_source_length);
 
 int main()
 {
     int N_input_lines;
     cin >> N_input_lines;
 
+    if (N_input_lines < 2)
+    {
+        cout << 0 << endl;
+
+        return 0;
+    }
+
     int *pointer_num_arr = new int[N_input_lines];
 
     readArray(pointer_num_arr, N_input_lines);
 
     const int abs_offset_length = getGeneralTerm(N_input_lines);
-    long long *pointer_abs_offset_values_arr = new long long[abs_offset_length];
+    int *pointer_abs_offset_values_arr = new int[abs_offset_length];
 
     calcAbsOffsetBetweenArrElems(pointer_num_arr, N_input_lines, pointer_abs_offset_values_arr);
 
-    long long closest_min_val = getMinIntValue(pointer_abs_offset_values_arr, abs_offset_length);
+    int closest_min_val = getMinIntValue(pointer_abs_offset_values_arr, abs_offset_length);
 
     cout << closest_min_val << endl;
 
@@ -34,6 +41,43 @@ int main()
 
     delete[] pointer_abs_offset_values_arr;
     pointer_abs_offset_values_arr = nullptr;
+
+    /*
+    //solution 1
+
+    int N_input_lines;
+    cin >> N_input_lines;
+
+    if (N_input_lines == 1)
+    {
+        cout << 0 << endl;
+
+        return 0;
+    }
+
+    int *pointer_dyn_arr = new int[N_input_lines];
+
+    readArray(pointer_dyn_arr, N_input_lines);
+
+    int min_diff = INT_MAX;
+    for (int curr_index = 0; curr_index < (N_input_lines - 1); curr_index++)
+    {
+        for (int inner = (curr_index + 1); inner < N_input_lines; inner++)
+        {
+            int curr_diff = abs(pointer_dyn_arr[curr_index] - pointer_dyn_arr[inner]);
+
+            if (curr_diff < min_diff)
+            {
+                min_diff = curr_diff;
+            }
+        }
+    }
+
+    cout << min_diff << endl;
+
+    delete[] pointer_dyn_arr;
+    pointer_dyn_arr = nullptr;
+    */
 
     return 0;
 }
@@ -54,16 +98,14 @@ int getGeneralTerm(int arr_source_length)
     return arr_length;
 }
 
-void calcAbsOffsetBetweenArrElems(int *point_arr_source, int arr_source_length, long long *point_destination_arr)
+void calcAbsOffsetBetweenArrElems(int *point_arr_source, int arr_source_length, int *point_destination_arr)
 {
     for (int i = 0; i < arr_source_length; i++)
     {
         for (int j = (i + 1); j < arr_source_length; j++)
         {
-            long long diff = (long long)point_arr_source[i] - (long long)point_arr_source[j];
-            long long curr_abs_offset = abs(diff);
+            int curr_abs_offset = abs(point_arr_source[i] - point_arr_source[j]);
             *(point_destination_arr) = curr_abs_offset;
-
             (point_destination_arr)++;
         }
     }
@@ -79,12 +121,12 @@ void printArr(int arr_source[], int arr_length)
     cout << endl;
 }
 
-long long getMinIntValue(long long arr_source[], int arr_source_length)
+int getMinIntValue(int arr_source[], int arr_source_length)
 {
-    long long min = LLONG_MAX;
+    int min = INT_MAX;
     for (int i = 0; i < arr_source_length; i++)
     {
-        long long curr_num = arr_source[i];
+        int curr_num = arr_source[i];
         if (curr_num < min)
         {
             min = curr_num;
